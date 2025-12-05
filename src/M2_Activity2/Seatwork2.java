34247eac4d70a6f8f429711131cda096de3bb97c
Juan Miguel A. Aquino <jmaaquino@headoffice.corp.bpi.com.ph>
2025-12-05 09:27:59 +0800

    add?

---
+++ src/M2_Activity2/Seatwork2.java
@@ -0,0 +1,10 @@
+package M2_Activity2;
+
+public class Seatwork2 {
+    public static void main(String[] args) {
+        WhiteBoard wb = new WhiteBoard();
+        int sum = wb.addNumbers(5, 3);
+        System.out.println("The total is: " + sum);
+    }
+    
+}
---
+++ src/M2_Activity2/WhiteBoard.java
@@ -0,0 +1,8 @@
+package M2_Activity2;
+
+public class WhiteBoard {
+    int addNumbers(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+}
